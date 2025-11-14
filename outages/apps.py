623711929e3a025724a09b26572d054dda38d027
ChickKelly <chick.kellyqueen@ictuniversity.edu.cm>
2025-11-14 17:15:01 +0100

    project

--- outages/apps.py
+++ outages/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
-
 class OutagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'outages'
+
+    def ready(self):
+        import sys
+        if 'runserver' in sys.argv:
+            from .mqtt_listener import start_mqtt
+            start_mqtt()
